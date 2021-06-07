2bf46cd816430c67bff00dbef9074975b22f8a09
tahir-kali <32597047+tahir-kali@users.noreply.github.com>
2021-06-07 15:08:41 +0430

    Update deflate_test.go

--- tests/deflate_test.go
+++ tests/deflate_test.go
@@ -9,9 +9,9 @@ import (
 	"time"
 
 	"github.com/gobwas/httphead"
-	"github.com/gobwas/ws"
-	"github.com/gobwas/ws/wsflate"
-	"github.com/gobwas/ws/wsutil"
+	"github.com/tahir-kali/tgws"
+	"github.com/tahir-kali/tgws/wsflate"
+	"github.com/tahir-kali/tgws/wsutil"
 )
 
 func TestFlateClientServer(t *testing.T) {
